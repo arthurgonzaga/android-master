bb436f79388bcdab3264e66e8da203616279d121
Arthur Gonzaga <42078278+arthurgonzaga@users.noreply.github.com>
2021-01-25 19:58:50 -0300

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,14 +15,14 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `content/blog`,
+        path: `${__dirname}/content/blog`,
         name: `blog`,
       },
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `content/assets`,
+        path: `${__dirname}/content/assets`,
         name: `assets`,
       },
     },
